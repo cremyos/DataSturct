a24f42531014dcc8416454a6e8a6525278717164
cremyos <dreamer_forever@126.com>
2016-06-28 21:23:20 +0800

    Topological

--- Graph/TopologicalSort.cpp
+++ Graph/TopologicalSort.cpp
@@ -11,8 +11,6 @@
 #include <string.h>
 
 using namespace std;
-//#include "Queue.cpp"
-//#include "Queue.h"
 
 #define ERROR -1
 #define OK 0
@@ -21,6 +19,11 @@ using namespace std;
 
 typedef int Status;
 
+int visited_AdjList[14] = {0};
+
+/**
+ * Graph Martix
+ */
 int graph[14][14] = {
 0, 0, 0, 0,  1, 1, 0, 0,  0, 0, 0, 1,  0, 0,
 0, 0, 1, 0,  1, 0, 0, 0,  1, 0, 0, 0,  0, 0,
@@ -28,11 +31,11 @@ int graph[14][14] = {
 0, 0, 1, 0,  0, 0, 0, 0,  0, 0, 0, 0,  0, 1,
 
 0, 0, 0, 0,  0, 0, 0, 1,  0, 0, 0, 0,  0, 0,
-0, 0, 0, 0,  0, 0, 0, 1,  0, 0, 0, 0,  0, 0,
 0, 0, 0, 0,  0, 0, 0, 0,  1, 0, 0, 0,  1, 0,
 0, 0, 0, 0,  0, 1, 0, 0,  0, 0, 0, 0,  0, 0,
-
 0, 0, 0, 0,  0, 0, 0, 0,  0, 0, 0, 0,  0, 0,
+
+0, 0, 0, 0,  0, 0, 0, 1,  0, 0, 0, 0,  0, 0,
 0, 0, 0, 0,  0, 0, 0, 0,  0, 0, 1, 1,  0, 0,
 0, 0, 0, 0,  0, 0, 0, 0,  0, 0, 0, 0,  0, 1,
 0, 0, 0, 0,  0, 0, 0, 0,  0, 0, 0, 0,  0, 0,
@@ -44,35 +47,37 @@ int graph[14][14] = {
 
 typedef struct EdgeNode
 {
-	int adjvex;
-	int weight;
-	struct EdgeNode *next;
+	int adjvex;				/// Adj vex
+	struct EdgeNode *next;	/// next point  
 }EdgeNode;
 
 typedef struct VertexNode
 {
-	int in;
-	int data;
-	EdgeNode *firstedge;
+	int in;					/// In-degree
+	int data;				/// data
+	EdgeNode *firstedge;	/// first edge node
 }VertexNode, AdjList[MAXVEX];
 
 typedef struct
 {
-	AdjList adjList;
-	int numVertexes, numEdges;
+	AdjList adjList;			/// adjlist
+	int numVertexes, numEdges;	///number of vertex and edge
 }graphAdjList, *GraphAdjList;
 
+/**
+ * TopologicalSort 
+ */
 Status TopologicalSort(GraphAdjList GL)
 {
 	EdgeNode *e;
-	int i, k, gettop;
+	int i, k, gettop = 0;
 	int top = 0;
 	int count = 0;
 	int *stack;
-	stack = (int *)malloc(GL->numVertexes * sizeof(int));
+	stack = (int *)malloc(MAXVEX * sizeof(int));
 	
-	for(i = 0; i < GL->numVertexes; i++)
-		if (GL->adjList[i].in == 0)
+	for(i = 0; i < MAXVEX; i++)
+		if (GL->adjList[i].in == 0) 
 			stack[++top] = i;
 	
 	while(top != 0) {
@@ -81,25 +86,46 @@ Status TopologicalSort(GraphAdjList GL)
 		count ++;
 		for(e = GL->adjList[gettop].firstedge; e; e = e->next) {
 			k = e->adjvex;
-			if(!(--GL->adjList[k].in))
+			if(!(--GL->adjList[k].in)) {
 				stack[++top] = k;
+			}
+				
 		}
 	}
-	if(count < GL->numVertexes)
+	if(count < MAXVEX)
 		return ERROR;
 	else 
 		return OK;
 }
 
+/**
+ * Create the Graph of AdjList
+ */
 void CreateALGraph(GraphAdjList G)
 {
 	int i, j, k;
 	EdgeNode *e;
+
 	for(i = 0; i < MAXVEX; i++) {
-		G->adjList[i].data = 'A'+i;
+		G->adjList[i].data = i;		
 		G->adjList[i].firstedge = NULL;
 	}
-
+	
+	G->adjList[0].in = 0;
+	G->adjList[1].in = 0;
+	G->adjList[2].in = 2;
+	G->adjList[3].in = 0;
+	G->adjList[4].in = 2;
+	G->adjList[5].in = 3;
+	G->adjList[6].in = 1;
+	G->adjList[7].in = 2;
+	G->adjList[8].in = 2;
+	G->adjList[9].in = 1;
+	G->adjList[10].in = 1;
+	G->adjList[11].in = 2;
+	G->adjList[12].in = 1;
+	G->adjList[13].in = 2;
+	
 	for(i = 0; i < MAXVEX; i++) {
 		for(j = 0; j < MAXVEX; j++) {
 			if(graph[i][j] == 1) {
@@ -107,12 +133,6 @@ void CreateALGraph(GraphAdjList G)
 				e->adjvex = j;
 				e->next = G->adjList[i].firstedge;
 				G->adjList[i].firstedge = e;
-
-                e = new EdgeNode;
-				e->adjvex = i;
-				e->next = G->adjList[j].firstedge;
-				G->adjList[j].firstedge = e;
-
 			}
 		}
 	}
@@ -122,8 +142,14 @@ void CreateALGraph(GraphAdjList G)
 int main()
 {
 	GraphAdjList GL;
-
-	CreateALGraph(GL);
+	int i = 0;
 	
+	GL = (GraphAdjList )malloc(sizeof(graphAdjList));
+	CreateALGraph(GL);
+	cout<<"TopologicalSort Result: "<<endl;
 	TopologicalSort(GL);
+	cout<<endl;
+	
+	free(GL);
+	return 0;
 }
